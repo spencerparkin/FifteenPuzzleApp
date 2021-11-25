4b4c216a6fc02e9970c21375ae133dc863faebe3
Spencer T. Parkin <spencertparkin@gmail.com>
2021-11-24 22:15:57 -0700

    Get basic UI setup in XAML.  It looks like crap, but we can get it functional before trying to make it look better.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,19 +6,16 @@ namespace FifteenPuzzle
 {
 	public partial class MainPage : ContentPage
 	{
-		int count = 0;
+		private int moveCount = 0;
 
 		public MainPage()
 		{
 			InitializeComponent();
 		}
 
-		private void OnCounterClicked(object sender, EventArgs e)
+		private void OnScrambleButtonClicked(object sender, EventArgs e)
 		{
-			count++;
-			CounterLabel.Text = $"Current count: {count}";
-
-			SemanticScreenReader.Announce(CounterLabel.Text);
+			
 		}
 	}
 }
